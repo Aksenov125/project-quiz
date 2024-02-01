8c61d1a096105df0be497d6339d36a3c6868dc6b
ZakharchenkoV <zvk096@gmail.com>
2024-02-01 16:04:36 +0300

    Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,23 @@
+const React = require('react');
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo right">
+          Quiz
+        </a>
+        <ul id="nav-mobile" className="left hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Темы</a>
+          </li>
+          <li>
+            <a href="badges.html">Выход</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+module.exports = Navbar;
